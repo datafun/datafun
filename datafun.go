d20906af0e224d5d68e254da2deee14d09e9dacb
JP Richardson <jprichardson@gmail.com>
2013-03-28 16:25:20 -0500

    add csv flag and horizontal flag

--- datafun.go
+++ datafun.go
@@ -3,6 +3,7 @@ package datafun
 import (
 	"os"
 	"fmt"
+	"path"
 	"github.com/jprichardson/commander-go"
 )
 
@@ -18,7 +19,14 @@ func AddInputOutputOptions(commander *commander.Commander) {
 	inputOption.Value = os.Stdin; //default
 	inputOption.Callback = func(args ...string) {
 		if len(args) > 0 {
-			val, err := os.Open(args[0])
+			fileName := args[0]
+			if (path.Ext(fileName) == ".csv") {
+				//hacky
+				AddCsvOption(commander)
+				commander.Opts["csv"].Value = true
+			}
+
+			val, err := os.Open(fileName)
 			inputOption.Value = val
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%s\n", err)
@@ -40,6 +48,27 @@ func AddInputOutputOptions(commander *commander.Commander) {
 		} else {
 			fmt.Fprintf(os.Stderr, "Must specify a parameter for -i or --input")
 		}
+	}
+}
+
+
+func AddHorizontalOption(commander *commander.Commander) {
+	commander.Option("-x, --horizontal", "perform operation horizontally instead of vertically")
+
+	horizontalOption := commander.Opts["horizontal"]
+	horizontalOption.Value = false
+	horizontalOption.Callback = func(args ...string) {
+		horizontalOption.Value = true
+	}
+}
+
+func AddCsvOption(commander *commander.Commander) {
+	commander.Option("-c, --csv", "specify CSV input, triggered by default if input filename ends in .csv")
+
+	option := commander.Opts["csv"]
+	option.Value = false
+	option.Callback = func(args ...string) {
+		option.Value = true
 	}
 }
 
