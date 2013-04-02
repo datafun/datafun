2df852691a0f1a6b78e6e1127b9bf95d0ea14a26
JP Richardson <jprichardson@gmail.com>
2013-04-02 02:11:35 -0500

    added tests for vertical/horizontal mean

--- test/aggregate/mean.test.sh
+++ test/aggregate/mean.test.sh
@@ -5,6 +5,7 @@ source ./test/testlib.sh
 test_file_start 
 
 
+### TEST: default input
 
 test_start "mean" "" "data.txt"
 
@@ -13,6 +14,40 @@ OUTPUT=$(diff $RET -<<EOF
 EOF)
 
 assert_empty $OUTPUT
+
+
+### TEST: csv vertical input
+
+#setup data for next test
+DATA="1,2,3,4\n5,6,7,8\n9,10,11,12\n13,14,15,16"
+TEST_FILE="$(pwd)/test/resources/test_mean.txt"
+printf $DATA > $TEST_FILE
+
+test_start "mean" "" "test_mean.txt"
+
+OUTPUT=$(diff $RET -<<EOF
+7,8,9,10
+EOF)
+
+assert_empty $OUTPUT
+
+
+### TEST: csv horizontal input
+
+test_start "mean" "-x" "test_mean.txt"
+
+OUTPUT=$(diff $RET -<<EOF
+2.5
+4.5
+6.5
+8.5
+EOF)
+
+assert_empty $OUTPUT
+
+
+rm $TEST_FILE
+
 
 
 
